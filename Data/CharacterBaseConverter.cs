e8748ec48a18143d83e629dd438948784c347a08
Samantha A Loveday <sloveday1@my.wctc.edu>
2024-10-10 19:47:55 -0500

    Update CharacterBaseConverter.cs

--- Data/CharacterBaseConverter.cs
+++ Data/CharacterBaseConverter.cs
@@ -26,6 +26,8 @@ public class CharacterBaseConverter : JsonConverter<CharacterBase>
                 "Player" => typeof(Player),
                 "Goblin" => typeof(Goblin),
                 "Ghost" => typeof(Ghost),
+                "Giant" => typeof(Giant),
+                "Gargoyle" => typeof(Gargoyle),
                 _ => throw new NotSupportedException($"Type {typeProperty} is not supported")
             };
             var character = (CharacterBase)JsonSerializer.Deserialize(root.GetRawText(), type, options);
